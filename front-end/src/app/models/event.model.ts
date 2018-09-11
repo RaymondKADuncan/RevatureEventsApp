f6e5bb31a162d48948c48690237e1f2f5b490849
RaymondKADuncan <raymond.k.a.duncan@gmail.com>
2018-09-11 13:36:42 -0400

    Added Angular code

---
+++ front-end/src/app/models/event.model.ts
@@ -0,0 +1,11 @@
+import { User } from './user.model';
+
+export class Event {
+    id: Number;
+    name: String;
+    description: String;
+    users: User[];
+    tags: String[];
+    groups: String[];
+    time: Date;
+}
---
+++ front-end/src/app/models/user.model.ts
@@ -0,0 +1,8 @@
+export class User {
+    username: String;
+    password: String;
+    firstname: String;
+    lastname: String;
+    role: String;
+    bio: String;
+}
