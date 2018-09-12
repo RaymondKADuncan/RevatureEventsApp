4d8575c8bae1483d998c1d9f475112f48d98c7ab
Ryan Posey <rkposey86@gmail.com>
2018-09-12 14:43:32 -0400

    working on event-list

--- front-end/src/app/components/event-list/event-list.component.ts
+++ front-end/src/app/components/event-list/event-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { Event } from '../../models/event.model';
 
 @Component({
   selector: 'app-event-list',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventListComponent implements OnInit {
 
-  constructor() { }
+  events: Event[] = [];
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+  }
+
+  getEvents() {
+    this.dataService.getAllEvents().subscribe(
+      e => {
+        this.events = e;
+      }
+    )
   }
 
 }
