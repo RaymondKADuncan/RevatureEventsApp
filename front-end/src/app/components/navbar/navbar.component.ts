e50e760be790987b4b1ab6fdac22beeeccc2e391
Giles <farringtong@wit.edu>
2018-09-13 16:03:16 -0400

    Some changes to login button

--- front-end/src/app/components/navbar/navbar.component.ts
+++ front-end/src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 
 import { AuthService } from '../../services/auth.service';
 import { ContextService } from '../../services/context.service';
 import { User } from '../../models/user.model';
+
+
 
 @Component({
   selector: 'app-navbar',
