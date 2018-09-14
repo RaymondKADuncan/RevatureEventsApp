6e4e1fb5d9831f78bed85facc19631c882e473a3
Joshua Hall <johall@clarkson.edu>
2018-09-14 13:19:40 -0400

    Searchbar routing

--- front-end/src/app/components/search/search.component.ts
+++ front-end/src/app/components/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 import { Event } from '../../models/event.model';
 
@@ -14,17 +15,18 @@ export class SearchComponent implements OnInit {
 
   searchParam = '';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
   }
 
   search(): void {
     //console.log('Searching for: ' + this.searchParam);
-    const url = 'http://ec2-54-208-38-230.compute-1.amazonaws.com:8080/RevEventApp/event/view/search/search?name=' + this.searchParam;
+    //const url = 'http://ec2-54-208-38-230.compute-1.amazonaws.com:8080/RevEventApp/event/view/search/search?name=' + this.searchParam;
     //const url = 'http://ec2-54-208-38-230.compute-1.amazonaws.com:8080/RevEventApp/event/search?param=' + this.searchParam;
-    const response = this.http.get<Event>(url);
-    response.subscribe(data => console.log(JSON.stringify(data)));
+    //const response = this.http.get<Event>(url);
+    //response.subscribe(data => console.log(JSON.stringify(data)));
+    this.router.navigateByUrl('event/view/search/search?name=' + this.searchParam);
   }
 
 }
