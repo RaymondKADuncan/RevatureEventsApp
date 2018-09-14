b70a6290ddd3cecc8ecdac2ae41d50e7394ce39c
Joshua Hall <johall@clarkson.edu>
2018-09-14 11:35:46 -0400

    Search bar progress

--- front-end/src/app/components/search/search.component.ts
+++ front-end/src/app/components/search/search.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+import { Event } from '../../models/event.model';
+
+@Injectable()
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -9,13 +14,16 @@ export class SearchComponent implements OnInit {
 
   searchParam = '';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
 
   search(): void {
     console.log('Searching for: ' + this.searchParam);
+    //let url = 'event/view/search/search?name=' + this.searchParam;
+    //let response = this.http.get<Event>(url);
+    //response.subscribe(r => console.log(r.id));
   }
 
 }
