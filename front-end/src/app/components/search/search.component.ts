2205f5135daaeff0750035ef5aad57ceebbdfa61
Joshua Hall <johall@clarkson.edu>
2018-09-14 11:56:51 -0400

    Search bar will now console.log a JSON with the search results.

--- front-end/src/app/components/search/search.component.ts
+++ front-end/src/app/components/search/search.component.ts
@@ -20,10 +20,11 @@ export class SearchComponent implements OnInit {
   }
 
   search(): void {
-    console.log('Searching for: ' + this.searchParam);
-    let url = 'event/view/search/search?name=' + this.searchParam;
+    //console.log('Searching for: ' + this.searchParam);
+    //let url = 'event/view/search/search?name=' + this.searchParam;//for deployed website
+    let url = 'http://ec2-54-208-38-230.compute-1.amazonaws.com:8080/RevEventApp/event/view/search/search?name=' + this.searchParam;//for localhost
     let response = this.http.get<Event>(url);
-    response.subscribe(r => console.log(r.id));
+    response.subscribe(data => console.log(JSON.stringify(data)));
   }
 
 }
