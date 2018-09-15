2b9c0c8ccd3402adc331fc0d94ad987ec247191d
Joshua Hall <johall@clarkson.edu>
2018-09-15 11:55:33 -0400

    Please commit your changes or stash them before you merge.

--- front-end/src/app/components/search/search.component.ts
+++ front-end/src/app/components/search/search.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
 
 import { Event } from '../../models/event.model';
+import { DataService } from '../../services/data.service';
 
 @Injectable()
 @Component({
@@ -13,20 +13,21 @@ import { Event } from '../../models/event.model';
 })
 export class SearchComponent implements OnInit {
 
-  searchParam = '';
+  searchQuery = '';
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
   }
 
   search(): void {
+    const response = this.dataService.getSearchResults(this.searchQuery);
     //console.log('Searching for: ' + this.searchParam);
     //const url = 'http://ec2-54-208-38-230.compute-1.amazonaws.com:8080/RevEventApp/event/view/search/search?name=' + this.searchParam;
-    const url = 'http://ec2-54-208-38-230.compute-1.amazonaws.com:8080/RevEventApp/event/search?param=' + this.searchParam;
-    const response = this.http.get<Event[]>(url);
-    response.subscribe(data => console.log(JSON.stringify(data)));
-    //this.router.navigateByUrl('event/view/search/search?name=' + this.searchParam);
+    //const url = 'http://ec2-54-208-38-230.compute-1.amazonaws.com:8080/RevEventApp/event/search?param=' + this.searchParam;
+    //const response = this.http.get<Event[]>(url);
+    response.subscribe(data => console.log(JSON.stringify(data[0])));
+
   }
 
 }
